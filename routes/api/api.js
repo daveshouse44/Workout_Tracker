f35f4714f38f5a4079b0d61b2457a097fa19d276
David Tierney <daveshouse44@hotmail.com>
2021-10-26 17:12:15 -0500

    Added route for creating an exercise

--- routes/api/api.js
+++ routes/api/api.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
 const Workout = require("../../models/workout");
 
+// Route to create a new exercise
+router.post("/", async ({ body }, res) => {
+  try {
+    const newWorkout = await Workout.create(body);
+    res.status(200).json(newWorkout);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 // Route to get workout and exercise math for duration
 router.get("/", async (req, res) => {
   try {
